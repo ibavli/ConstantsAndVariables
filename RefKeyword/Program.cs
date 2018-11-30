40405a1a3d36b8ba1098acd5ac59c2912b565d01
ibavli <ibrahim.bavli35@gmail.com>
2018-11-30 21:31:26 +0300

    ref keyword

---
+++ RefKeyword/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "message";
+            string message2 = "message2";
+
+            Console.WriteLine(message);
+            Console.WriteLine(message2);
+
+            //message değişkeni metoda ref parametresi ile gönderildiği için değeri değişti
+            WriteMessage(ref message, message2);
+
+            Console.WriteLine(message);
+            Console.WriteLine(message2);
+            Console.ReadLine();
+        }
+
+        private static void WriteMessage(ref string message, string message2)
+        {
+            message = "message metot tarafından değiştildi";
+            message2 = "message2 metot tarafından değiştildi";
+        }
+    }
+}
